79ca9cb565b0ee6deb27ab6db4c1160ef7e83215
nguyenyou <tunt081295@gmail.com>
2025-11-07 18:07:48 +0700

    🚢 ship it

--- examples/autogen/react/example1/src/Main.scala
+++ examples/autogen/react/example1/src/Main.scala
@@ -5,6 +5,18 @@ import com.raquo.laminar.api.L.*
 @main def app = {
   val container = dom.document.querySelector("#root")
   render(container, {
-    div("Hello, React!")
+    import scala.scalajs.js
+    import scala.scalajs.js.annotation.JSImport
+    
+    object clsxMod {
+      @js.native
+      @JSImport("clsx", "clsx")
+      def clsx(x: String): String = js.native
+    }
+    
+    val y = clsxMod.clsx("flex items-center justify-center")
+    println(y)
+    
+    div(y, "Hello, React!")
   })
 }
